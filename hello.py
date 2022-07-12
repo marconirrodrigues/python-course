5066ce3cad4359d61206c9f04d499a897f954a3b
marconirrodrigues <marconirodrigues.data@gmail.com>
2022-07-11 21:12:00 -0300

    Incluindo Docstring e metadados dunder

--- hello.py
+++ hello.py
@@ -1,3 +1,20 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
+"""O Nome Que Será Dado ao Programa.
 
-print('Hello World!')
+Descrição de como será o programa e o seu objetivo.
+
+Como usar:
+
+Especificação de como você vai usar o programa
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "1.0.0"
+__author__ = "Nome do Autor"
+__license__ = "Licenciado ou Licença Livre"
+
+
